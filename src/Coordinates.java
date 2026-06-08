60174cd6d7dfda3f837c1639472a63443ab95fce
Lyle11081 <lyle2539@gmail.com>
2026-06-08 10:39:34 +0100

    V 0.1

---
+++ src/Coordinates.java
@@ -0,0 +1,35 @@
+public class Coordinates {
+    //
+    private int x;
+    private int y;
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    // Set and Get X,Y
+    public int getX() {return x;}
+    public void setX(int x) {this.x = x;}
+    public int getY() {return y;}
+    public void setY(int y) {this.y = y;}
+    //Calculate Distance Between points
+    public double distance(Coordinates p) {
+        return Math.sqrt(Math.pow(this.x-p.x, 2)+Math.pow(this.y-p.y, 2));
+    }
+    public void translate(int dx, int dy) {
+        this.x += dx;
+        this.y += dy;
+    }
+    // Define valuables which positive or negative
+    public void scale(int factor, boolean sign){
+        if(sign == true){
+            this.x *= factor;
+            this.y *= factor;
+        }
+        else if(sign == false) {
+            this.x *= -factor;
+            this.y *= -factor;
+        }
+    }
+    // Display Coordinate
+    public String Display() {return String.format("(%d,%d)", x, y);}
+}
---
+++ src/Shape_list.java
@@ -0,0 +1,2 @@
+public class Shape_list {
+}
